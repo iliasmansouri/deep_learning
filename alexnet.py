20688d397a593dd24c9d5201dac05d399d78c1b5
Ilias <ilias.mansouri@keemotion.com>
2020-05-13 10:29:34 +0200

    refactor layers

--- alexnet.py
+++ alexnet.py
@@ -1,46 +1,36 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import logging
+
+import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader
-from torchvision.datasets import ImageFolder
 from torchvision import transforms
+from torchvision.datasets import ImageFolder
+
+from utils import DataSplit
 
 
-class AlexNet(nn.Module):
-    def __init__(self):
+class AlexNet(pl.LightningModule):
+    def __init__(self, path_to_data):
         super(AlexNet, self).__init__()
 
-        self.layer1 = self.conv_layer_with_LRN(3, 96, 11, 4)
-        self.layer2 = self.conv_layer_with_LRN(96, 256, 5, padding=2)
-
-        self.layer3 = self.conv_layer(256, 384, 3, padding=1)
-        self.layer4 = self.conv_layer(384, 384, 3, padding=1)
-
-        self.layer5 = nn.Sequential(
-            nn.Conv2d(384, 256, 3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-        )
+        self.path_to_data = path_to_data
 
         self.base_net = nn.Sequential(
-            self.layer1,
-            self.layer2,
-            self.layer3,
-            self.layer4,
-            self.layer5
+            self.conv_layer_with_LRN(3, 96, 11, 4),
+            self.conv_layer_with_LRN(96, 256, 5, padding=2),
+            self.conv_layer(256, 384, 3, padding=1),
+            self.conv_layer(384, 384, 3, padding=1),
+            self.conv_layer(384, 256, 3, padding=1),
+            nn.MaxPool2d(kernel_size=3, stride=2)
         )
 
-        self.fc1 = self.fc_layer(256*6*6, 4096)
-        self.fc2 = self.fc_layer(4096, 4096)
-        self.fc3 = self.fc_layer(4096, 3)
-
         self.head = nn.Sequential(
-            self.fc1,
-            self.fc2,
-            self.fc3
+            self.fc_layer(256*6*6, 4096),
+            self.fc_layer(4096, 4096),
+            self.fc_layer(4096, 3)
         )
 
     def fc_layer(self, in_features, out_features):
