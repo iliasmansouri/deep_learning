1bde5dd97ea67e009399bb9afe8147eb05cb27a2
Ilias <ilias.mansouri@keemotion.com>
2021-01-21 14:20:32 +0100

    vgg inherits from abstract class

--- models/image_classification/vgg16.py
+++ models/image_classification/vgg16.py
@@ -1,19 +1,17 @@
-import pytorch_lightning as pl
 import torch
 import torch.nn as nn
-from torchvision.datasets import ImageFolder
-from torchvision import transforms
 import torch.nn.functional as F
-
+from models.image_classification.abstract import ImageClassifier
 from models.nn_utils import conv_layer, fc_layer
-from data import DataSplit
 
 
-class VGG16(pl.LightningModule):
+class VGG16(ImageClassifier):
     def __init__(self, data_handler):
-        super(VGG16, self).__init__()
-        self.num_classes = data_handler.get_num_classes()
-        self.data_handler = data_handler
+        self.loss_function = F.cross_entropy
+        self.optimizer = torch.optim.Adam
+
+        ImageClassifier.__init__(self, data_handler, self.loss_function, self.optimizer)
+
         self.base_net = nn.Sequential(
             conv_layer(3, 64, 3, 1, 1),
             nn.MaxPool2d(2, 2),
@@ -40,23 +38,3 @@ class VGG16(pl.LightningModule):
         out = self.base_net(x)
         out = out.view(-1, 512 * 7 * 7)
         return self.head(out)
-
-    def prepare_data(self):
-        (
-            self.train_loader,
-            self.val_loader,
-            self.test_loader,
-        ) = self.data_handler.get_split()
-
-    def training_step(self, batch, batch_idx):
-        x, y = batch
-        y_hat = self(x)
-        loss = F.cross_entropy(y_hat, y)
-        tensorboard_logs = {"train_loss": loss}
-        return {"loss": loss, "log": tensorboard_logs}
-
-    def train_dataloader(self):
-        return self.train_loader
-
-    def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=0.02)
